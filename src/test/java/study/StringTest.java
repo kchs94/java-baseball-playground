e52f648afd8842e1ebffb686e7f72618768ca7ed
kchs94 <kchs94@gmail.com>
2022-05-18 16:38:11 +0900

    학습테스트 실습 구현 완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,4 +11,49 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split1() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1", "2");
+    }
+
+    @Test
+    void split2() {
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void subString() {
+        String str = "(1,2)";
+        String actual = str.substring(1, str.length()-1);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt() 테스트")
+    void charAt() {
+        // given
+        String str = "123";
+
+        // when
+        char actual = str.charAt(0);
+
+        // then
+        assertThat(actual).isEqualTo('1');
+    }
+
+    /*@Test
+    @DisplayName("charAt() 테스트")
+    void charAtException() {
+        // given
+        String str = "123";
+
+        // when
+        char actual = str.charAt(4);
+
+        // then
+        assertThat(actual).isEqualTo('1');
+    }*/
 }
