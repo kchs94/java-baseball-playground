a39c92f4a6635ba3f38a922412e3c066a5abb71a
kchs94 <kchs94@gmail.com>
2022-05-28 14:31:12 +0900

    입력 기능, 연산 기능, 출력 기능 main 메서드 밖으로 추출

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,29 +4,42 @@ import static java.lang.System.exit;
 
 public class Calculator {
 
-  public static void main(String[] args) {
+  static String[] input() {
     System.out.print("계산할 수를 입력하세요: ");
     Scanner in = new Scanner(System.in);
-    String[] temp = in.nextLine().split(" ");
+    return in.nextLine().split(" ");
+  }
+  static int calculate(int first, String operator, int second) {
+    int result = 0;
+    if (operator.equals("+")) {
+      result = first + second;
+    } else if (operator.equals("-")) {
+      result = first - second;
+    } else if (operator.equals("*")) {
+      result = first * second;
+    } else if (operator.equals("/")) {
+      result = first / second;
+    } else {
+      System.out.println("사칙연산이 아닙니다.");
+      exit(0);
+    }
+    return result;
+  }
+
+  static void print(int result) {
+    System.out.println("결과값: " + result);
+  }
+
+  public static void main(String[] args) {
+    String[] temp = input();
 
     int result = Integer.parseInt(temp[0]);
     for (int i=1; i<temp.length; i += 2) {
       String operator = temp[i];
       int second = Integer.parseInt(temp[i+1]);
-
-      if (operator.equals("+")) {
-        result = result + second;
-      } else if (operator.equals("-")) {
-        result = result - second;
-      } else if (operator.equals("*")) {
-        result = result * second;
-      } else if (operator.equals("/")) {
-        result = result / second;
-      } else {
-        System.out.println("사칙연산이 아닙니다.");
-        exit(0);
-      }
+      result = calculate(result, operator, second);
     }
-    System.out.println("결과값: " + result);
+
+    print(result);
   }
 }
